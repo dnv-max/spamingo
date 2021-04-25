de9505180f48ac467afb111afc0eee8283e2f030
dnv-max <81434516+dnv-max@users.noreply.github.com>
2021-04-25 05:30:34 -0400

    Update spamingo.py

--- spamingo.py
+++ spamingo.py
@@ -9,6 +9,6 @@ c = str(input("enter the message to spam : "))
 d = int(input("enter the number of seconds between each  message (keep it 0 for fastest spam)  : "))
 sleep(a)
 for z in range(1,b):
-  typewrite(d)
+  typewrite(c)
   press("enter")
   sleep(d)
