2cd3f47579c7e969a6e7f1ecd17713de6ce2aaf2
dnv-max <81434516+dnv-max@users.noreply.github.com>
2021-04-25 05:29:24 -0400

    Update spamingo.py

--- spamingo.py
+++ spamingo.py
@@ -1,8 +1,11 @@
 import pyautogui
 import time
+sleep = time.sleep
+typewrite = pyautogui.typewrite
+press = pyautogui.press
 a = int(input("enter the number of seconds to start the script : "))
 b = int(input("number of messages to spam : "))
-c = str(input("enter the message to spam"))
+c = str(input("enter the message to spam : "))
 d = int(input("enter the number of seconds between each  message (keep it 0 for fastest spam)  : "))
 sleep(a)
 for z in range(1,b):
