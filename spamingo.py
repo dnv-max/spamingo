23c9100e375ada2a465c81a7e4346192d4810611
dnv-max <81434516+dnv-max@users.noreply.github.com>
2021-04-26 04:59:23 -0400

    Update spamingo.py

--- spamingo.py
+++ spamingo.py
@@ -1,10 +1,10 @@
 import pyautogui
 import time
-println(" _  ___           _                 _       _     ")
-println("| |/ (_)_ __   __| | __ _  ___ __ _| |_ ___| |__  ")
-println("| ' /| | '_ \ / _` |/ _` |/ __/ _` | __/ __| '_ \ ")
-println("| . \| | | | | (_| | (_| | (_| (_| | || (__| | | |")
-println("|_|\_\_|_| |_|\__,_|\__,_|\___\__,_|\__\___|_| |_|")
+print(" _  ___           _                 _       _     ")
+print("| |/ (_)_ __   __| | __ _  ___ __ _| |_ ___| |__  ")
+print("| ' /| | '_ \ / _` |/ _` |/ __/ _` | __/ __| '_ \ ")
+print("| . \| | | | | (_| | (_| | (_| (_| | || (__| | | |")
+print("|_|\_\_|_| |_|\__,_|\__,_|\___\__,_|\__\___|_| |_|")
 a = int(input("enter the number of seconds to start the script : "))
 b = int(input("number of messages to spam : "))
 c = str(input("enter the message to spam : "))
